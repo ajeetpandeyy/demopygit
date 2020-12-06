727f1e29a33c78ff112da4f44e661bf4e977dc48
Ajeet <aitajeet@gmail.com>
2020-12-06 14:29:49 +0530

    Started

--- BestHelthPracticeManagement.py
+++ BestHelthPracticeManagement.py
@@ -4,3 +4,19 @@
 #                                                   #
 ####################################################
 
+print(" Healthy Programmer Reminder System  ")
+
+from pygame import mixer
+
+def musiconloop(file, stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play(3)
+    while True:
+        a = input()
+        if a == stopper:
+            mixer.music.stop()
+            break
+
+if __name__ == '__main__':
+    musiconloop("water.mp3","stop")
